4de53cfc83222d793720cb02b60a3e6d27752cd5
Slava Sidorov <sidorov-vv3@mail.ru>
2025-11-24 03:11:40 +0500

    test deploy

--- functions.php
+++ functions.php
@@ -74,18 +74,174 @@
 
 		return $args;
 	}
-	
-    add_filter('woocommerce_product_add_to_cart_text', 'change_button_for_category');
-    add_filter('woocommerce_product_single_add_to_cart_text', 'change_button_for_category');
 
-    function change_button_for_category($text) {
-        global $product;
-        
-        if (has_term('delivery', 'product_cat', $product->get_id())) {
-            return 'Выставить счёт';
-        }
-        
-        return $text;
+add_theme_support('woocommerce');
+
+add_action('wp_ajax_update_cart_item', 'update_cart_item_ajax');
+add_action('wp_ajax_nopriv_update_cart_item', 'update_cart_item_ajax');
+
+function update_cart_item_ajax() {
+    if (!defined('WOOCOMMERCE_CART')) {
+        define('WOOCOMMERCE_CART', true);
     }
-	
+    
+    WC()->frontend_includes();
+    if (is_null(WC()->cart)) {
+        wc_load_cart();
+    }
+    
+    $cart_item_key = sanitize_text_field($_POST['cart_item_key']);
+    $quantity = intval($_POST['quantity']);
+    
+    if ($quantity <= 0) {
+        WC()->cart->remove_cart_item($cart_item_key);
+    } else {
+        WC()->cart->set_quantity($cart_item_key, $quantity, true);
+    }
+    
+    WC()->cart->calculate_totals();
+    
+    // Получаем обновленные данные
+    $cart_item = WC()->cart->get_cart_item($cart_item_key);
+    $item_total_html = '';
+    
+    if ($cart_item) {
+        $item_total_html = WC()->cart->get_product_subtotal($cart_item['data'], $cart_item['quantity']);
+    }
+    
+    wp_send_json_success(array(
+        'cart_total' => WC()->cart->get_total(),
+        'cart_count' => WC()->cart->get_cart_contents_count(),
+        'item_total_html' => $item_total_html
+    ));
+}
+
+add_action('wp_ajax_quick_checkout', 'quick_checkout_ajax');
+add_action('wp_ajax_nopriv_quick_checkout', 'quick_checkout_ajax');
+
+function quick_checkout_ajax() {
+    $order = wc_create_order();
+    
+    foreach (WC()->cart->get_cart() as $cart_item) {
+        $order->add_product($cart_item['data'], $cart_item['quantity']);
+    }
+    
+    $order->set_address(array(
+        'first_name' => sanitize_text_field($_POST['billing_first_name']),
+        'email' => sanitize_email($_POST['billing_email']),
+        'phone' => sanitize_text_field($_POST['billing_phone']),
+    ), 'billing');
+    
+    $order->set_payment_method('robokassa');
+    $order->calculate_totals();
+    $order->save();
+    
+    WC()->cart->empty_cart();
+    
+    // Получаем gateway Robokassa и генерируем прямую ссылку на оплату
+    $gateways = WC()->payment_gateways->get_available_payment_gateways();
+    if (isset($gateways['robokassa'])) {
+        $robokassa = $gateways['robokassa'];
+        $payment_url = $robokassa->process_payment($order->get_id());
+        
+        if (isset($payment_url['redirect'])) {
+            wp_send_json_success(array('redirect' => $payment_url['redirect']));
+            return;
+        }
+    }
+    
+    // Если не получилось - стандартная страница
+    wp_send_json_success(array('redirect' => $order->get_checkout_payment_url()));
+}
+add_action('wp_footer', 'auto_submit_robokassa_payment');
+function auto_submit_robokassa_payment() {
+    if (is_wc_endpoint_url('order-pay') && isset($_GET['auto_submit'])) {
+        ?>
+        <script>
+        jQuery(document).ready(function($) {
+            setTimeout(function() {
+                $('#place_order').click();
+            }, 1000);
+        });
+        </script>
+        <?php
+    }
+}
+function custom_add_to_cart_text( $text ) {
+    return 'Купить';
+}
+add_filter( 'woocommerce_product_add_to_cart_text', 'custom_add_to_cart_text' );
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'custom_add_to_cart_text' );
+
+
+
+// Отправка письма администратору при успешной оплате
+add_action('woocommerce_order_status_processing', 'send_admin_email_on_payment', 10, 1);
+add_action('woocommerce_order_status_completed', 'send_admin_email_on_payment', 10, 1);
+
+function send_admin_email_on_payment($order_id) {
+    $order = wc_get_order($order_id);
+    
+    if ($order->get_meta('_admin_email_sent')) {
+        return;
+    }
+    
+    $admin_emails = array(
+        'vasilyev-r@mail.ru',
+        'sidorov-vv3@mail.ru'
+    );
+    
+    $subject = '=?utf-8?B?' . base64_encode('Новый оплаченный заказ #' . $order->get_order_number()) . '?=';
+    
+    $message = "Получен новый оплаченный заказ!\n\n";
+    $message .= "Номер заказа: #" . $order->get_order_number() . "\n";
+    $message .= "Дата: " . $order->get_date_created()->format('d.m.Y H:i') . "\n";
+    $message .= "Сумма: " . strip_tags($order->get_formatted_order_total()) . "\n\n";
+    
+    $message .= "КЛИЕНТ:\n";
+    $message .= "Имя: " . $order->get_billing_first_name() . "\n";
+    $message .= "Email: " . $order->get_billing_email() . "\n";
+    $message .= "Телефон: " . $order->get_billing_phone() . "\n\n";
+    
+    $message .= "ТОВАРЫ:\n";
+    foreach ($order->get_items() as $item) {
+        $message .= "- " . $item->get_name() . " x" . $item->get_quantity() . " = " . strip_tags($order->get_formatted_line_subtotal($item)) . "\n";
+    }
+    
+    $message .= "\nИТОГО: " . strip_tags($order->get_formatted_order_total()) . "\n\n";
+    $message .= "Ссылка на заказ: " . admin_url('post.php?post=' . $order_id . '&action=edit') . "\n";
+    
+    foreach ($admin_emails as $email) {
+        mail($email, $subject, $message);
+    }
+    
+    $order->update_meta_data('_admin_email_sent', 'yes');
+    $order->save();
+}
+
+
+add_filter('woocommerce_product_add_to_cart_text', 'change_button_for_category', 99);
+add_filter('woocommerce_product_single_add_to_cart_text', 'change_button_for_category', 99);
+
+function change_button_for_category($text) {
+    global $product;
+    
+    if (!$product) return $text;
+    
+    // Получаем все категории товара
+    $terms = get_the_terms($product->get_id(), 'product_cat');
+    
+    if ($terms) {
+        foreach ($terms as $term) {
+            if ($term->slug == 'delivery') {
+                return 'Выставить счёт';
+            }
+        }
+    }
+    
+    return $text;
+}
+
 ?>
+
+
